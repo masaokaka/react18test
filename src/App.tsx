775fe06811dc1b79476eba161b58031f5c1bb738
masaokaka <osushiiisensu@gmail.com>
2022-06-01 22:39:46 +0900

    update develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState, Suspense,useMemo } from 'react';
 import './App.css'
+import { RenderingNotifier } from './RenderingNotifier'
 
 function sleep (ms: number) {
-  return new Promise(resolve=>setTimeout(resolve,ms))
+  return new Promise(resolve =>  setTimeout(resolve,ms))
 }
 
 const AlwaysSuspended: React.VFC = () => {
   console.log("aaaa");
   throw sleep(1000);
 }
-ss
 
 const SometimeSuspended:React.VFC = () => {
   if (Math.random() < 0.5) {
@@ -18,14 +18,45 @@ const SometimeSuspended:React.VFC = () => {
   return <p>Hello World!!</p>
 }
 
+async function fetchData1(): Promise<string> {
+  await sleep(1000);
+  return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
+}
+
+ export const DataLoader: React.VFC = () => {
+   const [loading, setLoading] = useState(false);
+   const [data, setData] = useState<string | null>(null);
+
+  const _ = useMemo(() => {
+    if (loading) {
+      console.log("loading is true");
+    }
+    return 1;
+  }, [loading]);
+
+   // ローディングフラグが立っていてdataがまだ無ければローディングを開始する
+   if (loading && data === null) {
+     throw fetchData1().then(setData);
+   }
+   // データがあればそれを表示
+   return (
+     <div>
+       <div>Data is {data}</div>
+       <button className="border p-1" onClick={() => setLoading(true)}>
+         load
+       </button>
+     </div>
+   );
+ };
+
+
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
       <Suspense fallback={<p>...loading</p>}>
-        <SometimeSuspended />
-        <button className='border p-1' onClick={()=>setCount((c)=> c + 1)}>{count}</button>
+        <DataLoader/>
       </Suspense>
     </div>
   )
---
+++ src/RenderingNotifier.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+export const RenderingNotifier: React.VFC<Props> = ({ name }) => {
+  console.log(`${name} is rendered`);
+
+  return null;
+};
